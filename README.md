6e09fd0ae560ba2b19d2a4ce3981e19631b16600
GrigoreDenis <116000531+GrigoreDenis@users.noreply.github.com>
2022-11-02 16:34:06 +0200

    First use of SYN-Scan
    Created SYN-Scan, implemented GLOBAL TIMEOUT

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+ Make sure the first argument is the network you're trying to scan, examples: 142.250.181.238 or 142.250.181.0/32
+ Use -logs in order to see more details as the program runs...
+ Use -tcs | -ths | -udp | -sth in order to define a type of scanning...
+ Use -p to define a range of ports, examples: -p80,443 (commas) or -p1-100 (hyphens) or -p1-100,400-500 (hybrid)
+
+-tcs -> TCP Connect Scan
+-ths -> TCP Half-open Scan
+-udp -> UDP scan
+-sth -> Stealth Scan
+
 1) Pings the IP to make sure it's alive
     if it is NOT online 
         -> throws appropriate message
@@ -26,7 +36,7 @@ STEALTH SCANNING – NULL, FIN, X-MAS.
 
 One of the more common and popular port scanning techniques is the TCP half-open port scan, sometimes referred to as an SYN scan. It's a fast and sneaky scan that tries to find potential open ports on the target computer. SYN packets request a response from a computer, and an ACK packet is a response
 
-type of tcp scan
+Types of tcp scan defined in global variable:
 -1 not defined
 0 use half-connect TCP scan
 1 use full-connect TCP scan
--- custom_port_scanner.py
+++ custom_port_scanner.py
@@ -1,4 +1,7 @@
 # modules
+from concurrent.futures import ThreadPoolExecutor
+import sys
+from io import StringIO
 import logging
 import ipaddress
 import sys
@@ -7,13 +10,18 @@ import os
 from socket import AF_INET
 from socket import SOCK_STREAM
 from socket import socket
-
+from scapy.layers.inet import Ether, IP, TCP, ICMP
+from scapy.sendrecv import send, sr1
 # get network address from arguments and validate it
 
-network=sys.argv[1]
+try:
+    network=sys.argv[1]
+except:
+    print("ERROR: Please introduce the network first!")
+    quit()
 ports = []
 type_of_scan = -1
-
+GLOBAL_TIME_OUT = 5
 # -1 not defined
 # 0 use half-connect TCP scan
 # 1 use full-connect TCP scan
@@ -26,9 +34,20 @@ if "-h" in sys.argv:
     print("Use -logs in order to see more details as the program runs...")
     print("Use -tcs | -ths | -udp | -sth in order to define a type of scanning...")
     print("Use -p to define a range of ports, examples: -p80,443 (commas) or -p1-100 (hyphens) or -p1-100,400-500 (hybrid)")
+    print("Use -timeout=# to define a global timeout to be used when scanning: \n \tLonger timeout => slower but higher chance of detection\n \tShorter timeout => faster but lower chance of detection")
     quit()
 
+
+def check_scan_args():
+    if type_of_scan!=-1:
+        print("ERROR: Please use only one scan type")
+
+if "-ths" in sys.argv:
+    check_scan_args()
+    type_of_scan=0
+
 if "-tcs" in sys.argv:
+    check_scan_args()
     type_of_scan=1
 
 if type_of_scan == -1:
@@ -45,6 +64,10 @@ def log(string):
         print("LOG: %s" % string)
 
 
+for port_argument in sys.argv:
+    if "-timeout=" in port_argument:
+        GLOBAL_TIME_OUT = int(port_argument.split("=")[1])
+        print("Setting global timeout to: %d" % GLOBAL_TIME_OUT)
 
 #PORT OPTIONS: -p80,443 (commas) or -p1-100 (hyphens) or -p1-100,400-500 (hybrid)
 port_argument_found = False
@@ -89,20 +112,53 @@ def ThreadPing(host,t_id):
 
 # returns True if a connection can be made, False otherwise
 def test_port_number(host,host_index, port,port_index):
+####SYN-SCAN
     # create and configure the socket
-    log("TCP full connect scanning port number: %s in host: %s" % (port,host))
-    with socket(AF_INET, SOCK_STREAM) as sock:
-        # set a timeout of a few seconds
-        sock.settimeout(3)
-        # connecting may fail
+    if type_of_scan==0:
+        log("TCP half connect scanning host: %s" % host)
+        ip=IP(dst=str(host))
+        log("Attempting SYN Scan on port %s" % ports[port_index])
+        tcp=TCP(dport=int(ports[port_index])) # defaults to flags: S for SYN
+        packet = ip/tcp
+        result=sr1(packet, timeout =GLOBAL_TIME_OUT,iface="eth0",verbose=False)
+        #includes flags: SA for SYN-ACK
+        #Redirect output of print to variable 'capture'
         try:
-            # attempt to connect
-            sock.connect((host, int(port)))
-            # a successful connection was made
-            log("Succesfully connected to port: %s" % port)
-            port_results[host_index][port_index]=port
+            answered = result[0]
+            #unanswered = result[1]
+            #capture = StringIO()
+            #save_stdout = sys.stdout
+            #sys.stdout = capture
+            #answered.show()
+            #sys.stdout = save_stdout
+            capture = answered.show(dump=True)
+            if "SA" in capture:
+                port_results[host_index][port_index] = port
+                log("Host %s /Port: %s responded with SYN-ACK"% (host,port))
+            if "R" in capture:
+                log("Host %s /Port: %s did not respond with SYN-ACK"% (host,port))
         except:
+            log("Host %s /Port: %s did not respond at all"% (host,port,))
             pass
+####CONNECT-SCAN
+    elif type_of_scan==1:
+        log("TCP full connect scanning port number: %s in host: %s" % (port,host))
+        with socket(AF_INET, SOCK_STREAM) as sock:
+            # set a timeout of a few seconds
+            sock.settimeout(GLOBAL_TIME_OUT)
+            # connecting may fail
+            try:
+                # attempt to connect
+                sock.connect((host, int(port)))
+                # a successful connection was made
+                log("Host Succesfully connected to port: %s" % port)
+                port_results[host_index][port_index]=port
+            except:
+                log("Host failed to connect to port: %s" % port)
+                pass
+    else:
+        print("ERROR: type of scan undefined/wrong")
+        quit()
 
 
 
@@ -164,8 +220,6 @@ for i in range (len(results)):
         print(str(hosts[i]), "is Online")
 log("Ending ping probing")
 #if ports is empty, use default 1000 ports
-print("Executing Full-Connection Scan")
-
 
 if ports:
     print("Using custom ports")
@@ -177,9 +231,21 @@ else:
 
 port_results = [[0]*len(ports)]*len(hosts)
 
+##############################################################START TCP HALF CONNECT (SYN) SCAN
+if type_of_scan==0:
+    print("Executing Half-Connection (SYN) Scan")
+    #executor = ThreadPoolExecutor(max_workers=2)
+    for host_index in range (len(hosts)):
+        thread = threading.Thread(target=scan, args=(str(hosts[host_index]),host_index,ports,))
+        threads.append(thread)
+        thread.start()
+    for i,thread in enumerate(threads):
+        thread.join()
+        
 
+##############################################################START TCP FULL CONNECT SCAN
 if type_of_scan==1:
-    ###START TCP FULL CONNECT SCAN
+    print("Executing Full-Connection Scan")
     for host_index in range (len(hosts)):
         thread = threading.Thread(target=scan, args=(str(hosts[host_index]),host_index,ports,))
         threads.append(thread)
@@ -196,6 +262,3 @@ for host_index in range (len(hosts)):
     for port_index in range (len(ports)):
         if port_results[host_index][port_index]:
             print("%s\tOPEN\t...TODO...\t" % ports[port_index])
-
-
-
